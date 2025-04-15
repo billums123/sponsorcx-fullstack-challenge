cfc0c08e4e0d4241a1671a7cefc38aec97fcdcee
Tanner <tannerh@passivelogic.com>
2025-04-15 09:51:53 -0600

    feat: delete existing data when seeding

--- server/seed.ts
+++ server/seed.ts
@@ -3,6 +3,11 @@ import initializeDatabase from "./db";
 const db = initializeDatabase();
 
 // Function for seeding db
-function seed() {}
+function seed() {
+  // Clear existing data from db
+  db.prepare(`DELETE FROM deals`).run();
+  db.prepare(`DELETE FROM accounts`).run();
+  db.prepare(`DELETE FROM organizations`).run();
+}
 
 seed();
