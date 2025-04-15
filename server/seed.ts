0ef393d9793988569e19a08185199f7e9422aded
Tanner <tannerh@passivelogic.com>
2025-04-15 10:11:09 -0600

    feat: create random accounts for each org in seed script

--- server/seed.ts
+++ server/seed.ts
@@ -2,8 +2,13 @@ import initializeDatabase from "./db";
 import { faker } from "@faker-js/faker";
 
 // Default values for seeding script **ADJUST AS NEEDED**
+// organizations
 const MIN_ORGS = 2;
 const MAX_ORGS = 10;
+
+// accounts
+const MIN_ACCOUNTS = 1;
+const MAX_ACCOUNTS = 5;
 
 const db = initializeDatabase();
 
@@ -17,12 +22,25 @@ function seed() {
 
   // sql statements
   const insertOrg = db.prepare(`INSERT INTO organizations (name) VALUES (?)`);
+  const insertAccount = db.prepare(
+    `INSERT INTO accounts (name, organization_id) VALUES (?, ?)`
+  );
 
-  const numOrgs = faker.number.int({ min: 2, max: 5 });
+  const numOrgs = faker.number.int({ min: MIN_ORGS, max: MAX_ORGS });
 
   for (let o = 0; o < numOrgs; o++) {
     const orgName = faker.company.name();
     const org = insertOrg.run(orgName);
+    const orgId = org.lastInsertRowid as number;
+
+    const numAccounts = faker.number.int({
+      min: MIN_ACCOUNTS,
+      max: MAX_ACCOUNTS,
+    });
+    for (let a = 0; a < numAccounts; a++) {
+      const accountName = faker.company.name();
+      const account = insertAccount.run(accountName, orgId);
+    }
   }
 }
 
