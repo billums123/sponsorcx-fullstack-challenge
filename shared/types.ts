2b5272fed3cc527ecddb9d4e0b47c2c3be93ae89
Tanner <tannerh@passivelogic.com>
2025-04-15 21:03:04 -0600

    chore: add ts interfaces for all db tables

--- shared/types.ts
+++ shared/types.ts
@@ -5,3 +5,30 @@ export enum DealStatus {
   CLOSED_WON = "closed_won",
   CLOSED_LOST = "closed_lost",
 }
+
+export interface Organization {
+  id: number;
+  name: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Account {
+  id: number;
+  name: string;
+  organization_id: number;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Deal {
+  id: number;
+  name: string;
+  account_id: number;
+  start_date: string;
+  end_date: string;
+  value: number;
+  status: DealStatus;
+  created_at: string;
+  updated_at: string;
+}
