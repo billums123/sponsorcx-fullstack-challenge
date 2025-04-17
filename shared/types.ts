47a53ec8f71bf5ff72ec7af181d2764f7e311e1c
Tanner <tannerh@passivelogic.com>
2025-04-17 08:06:25 -0600

    chore: simplify DealStatus enum

--- shared/types.ts
+++ shared/types.ts
@@ -2,8 +2,6 @@ export enum DealStatus {
   BUILD = "build",
   PITCH = "pitch",
   NEGOTIATION = "negotiation",
-  CLOSED_WON = "closed_won",
-  CLOSED_LOST = "closed_lost",
 }
 
 export interface Organization {
