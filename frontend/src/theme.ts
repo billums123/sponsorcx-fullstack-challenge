f8bda582ea5f0c9352a67f4bd5e0366373c7e336
Tanner <tannerh@passivelogic.com>
2025-04-17 08:00:08 -0600

    feat: setup basic theming using MUI for frontend

---
+++ frontend/src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#53b0fd", // Color pulled from SponsorCX logo
+    },
+    secondary: {
+      main: "#f2f2f2",
+    },
+  },
+});
+
+export default theme;
