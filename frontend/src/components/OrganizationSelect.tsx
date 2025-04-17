607009d3f6734e39d16350829fd9810e9823fff3
Tanner <tannerh@passivelogic.com>
2025-04-17 09:03:57 -0600

    feat: create OrganizationSelect for selecting which org to show the deals of

---
+++ frontend/src/components/OrganizationSelect.tsx
@@ -0,0 +1,46 @@
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import { useFetch } from "../hooks/useFetch";
+import { fetchOrganizations } from "../api/organizations";
+import type { Organization } from "../../../shared/types";
+
+interface OrganizationSelectProps {
+  selectedOrg: number | "";
+  onChange: (value: number) => void;
+}
+
+// Dropdown that lists every organization in the DB
+export default function OrganizationSelect({
+  selectedOrg,
+  onChange,
+}: OrganizationSelectProps) {
+  const { data: orgs, loading, error } = useFetch(fetchOrganizations);
+
+  const handleChangeSelect = (e: SelectChangeEvent) => {
+    onChange(Number(e.target.value));
+  };
+
+  return (
+    <FormControl size="small" sx={{ minWidth: 200 }}>
+      <InputLabel id="org-label">Organization</InputLabel>
+      <Select
+        labelId="org-label"
+        value={String(selectedOrg)}
+        label="Organization"
+        onChange={handleChangeSelect}
+        disabled={loading || !!error}
+      >
+        {orgs?.map((o: Organization) => (
+          <MenuItem key={o.id} value={o.id}>
+            {o.name}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
